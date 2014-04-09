486a7fc5650c6bb95bd8c405a1e30c1c546a06de
Brian Emil Hartz <brianhartz@gmail.com>
2014-04-09 12:11:58 -0600

    have input and text switching

--- js/main.js
+++ js/main.js
@@ -4,9 +4,29 @@ $(document).on('ready', function() {
 
 var $inputTxt = $('<input>', {
     type: 'text',
-    class: 'user-input'
+    id: 'user-input'
 })
 
+
+var inputSwitch = function(element) {
+    if ($(element).attr('id') === 'user-input') {
+        $(element).parent().children('.editable').text($(element).val());
+
+    } else {
+        console.log($(element).parent());
+        console.dir($(element).parent());
+        $inputTxt.val($(element).text());
+        $(element).parent().append(($inputTxt));
+        // $(element).css('display', 'none');
+        $inputTxt.focus();
+    }
+}
+
 $('.editable').on('click', function() {
-    $(this).append($inputTxt);
+    inputSwitch(this);
+});
+
+$inputTxt.focusout(function() {
+    // inputSwitch(this);
+    $(this).parent().children('.editable').text($(this).val());
 });

05031af173a04dcd36a4d5d08e4831579bf3e661
Brian Emil Hartz <brianhartz@gmail.com>
2014-04-09 15:35:31 -0600

    put all js code inside the onReady event

--- js/main.js
+++ js/main.js
@@ -1,33 +1,29 @@
 $(document).on('ready', function() {
 
-
-
+    //putting event inside onReady
     $('.editable').on('click', function() {
         inputSwitch(this);
     });
 
+    var inputReSwitch = function(element) {
+        $(element).parent().children('.editable').text($(element).val());
+        $(element).parent().children('.editable').show();
+        $(element).remove();
+    }
 
+    var inputSwitch = function(element) {
+
+        var $inputTxt = $('<input>', {
+            type: 'text',
+            id: 'user-input'
+        })
+        $inputTxt.val($(element).text());
+        $(element).parent().append(($inputTxt));
+        $(element).hide();
+        $inputTxt.focus();
+        $('#user-input').blur(function() {
+            inputReSwitch(this);
+        });
+    }
 
 });
-
-
-var inputReSwitch = function(element) {
-    $(element).parent().children('.editable').text($(element).val());
-    $(element).parent().children('.editable').show();
-    $(element).remove();
-}
-
-var inputSwitch = function(element) {
-
-    var $inputTxt = $('<input>', {
-        type: 'text',
-        id: 'user-input'
-    })
-    $inputTxt.val($(element).text());
-    $(element).parent().append(($inputTxt));
-    $(element).hide();
-    $inputTxt.focus();
-    $('#user-input').blur(function() {
-        inputReSwitch(this);
-    });
-}

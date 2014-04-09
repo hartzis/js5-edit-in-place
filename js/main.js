e2fd16319612f1fe4fe716322d936adca798201f
Brian Emil Hartz <brianhartz@gmail.com>
2014-04-09 14:49:17 -0600

    changed dislplay:none, now using hide/show. also add blur()

--- js/main.js
+++ js/main.js
@@ -2,34 +2,30 @@ $(document).on('ready', function() {
 
 });
 
-var $inputTxt = $('<input>', {
-    type: 'text',
-    id: 'user-input'
-})
 
+var inputReSwitch = function(element) {
+    $(element).parent().children('.editable').text($(element).val());
+    $(element).parent().children('.editable').show();
+    $(element).remove();
+}
 
 var inputSwitch = function(element) {
-    if ($(element).attr('id') === 'user-input') {
-        $(element).parent().children('.editable').text($(element).val());
-        $(element).parent().children('.editable').css('display', 'block');
-        $(element).css('display', 'none');
 
-    } else {
-        console.log($(element).parent());
-        console.dir($(element).parent());
-        $inputTxt.val($(element).text());
-        $(element).parent().append(($inputTxt));
-        $inputTxt.css('display', 'inline-block');
-        $(element).css('display', 'none');
-        $inputTxt.focus();
-    }
+    var $inputTxt = $('<input>', {
+        type: 'text',
+        id: 'user-input'
+    })
+    console.log($(element).parent());
+    console.dir($(element).parent());
+    $inputTxt.val($(element).text());
+    $(element).parent().append(($inputTxt));
+    $(element).hide();
+    $inputTxt.focus();
+    $('#user-input').blur(function() {
+        inputReSwitch(this);
+    });
 }
 
 $('.editable').on('click', function() {
     inputSwitch(this);
-});
-
-$inputTxt.focusout(function() {
-    inputSwitch(this);
-    // $(this).parent().children('.editable').text($(this).val());
 });
